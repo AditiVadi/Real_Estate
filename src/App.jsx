0a2eed48c4571b3dcddc3e425e5456435a5edc67
Aditi-R-Vadi <vadiaditi@gmail.com>
2024-05-18 23:29:50 +0530

    Header comp

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,26 @@
 import { useState } from 'react'
 import React from 'react'
-import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
 import Signin from './pages/Signin'
 import Signout from './pages/Signout'
 import Profile from './pages/Profile'
+import Header from './Components/Header'
 function App() {
- 
+
 
   return (
     <BrowserRouter>
-    <Routes>
-    <Route path='/' element={<Home/>}/>
-    <Route path='/About' element={<About/>}/>
-    <Route path='/Sign-In' element={<Signin/>}/>
-    <Route path='/Sign-Out' element={<Signout/>}/>
-    <Route path='/Profile' element={<Profile/>}/>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/About' element={<About />} />
+        <Route path='/Sign-In' element={<Signin />} />
+        <Route path='/Sign-Out' element={<Signout />} />
+        <Route path='/Profile' element={<Profile />} />
 
-    </Routes>
+      </Routes>
     </BrowserRouter>
   )
 }
