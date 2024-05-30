f1c03e43c93ff1870a0ecbde23ec6f88990d7af7
Aditi-R-Vadi <vadiaditi@gmail.com>
2024-05-30 15:26:04 +0530

    fix discountPrice

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -6,6 +6,7 @@ import { useSelector } from 'react-redux';
 import { Navigation } from 'swiper/modules';
 import 'swiper/css/bundle';
 import { FaRupeeSign } from 'react-icons/fa';
+import React from 'react';
 
 import {
   FaBath,
@@ -88,14 +89,23 @@ export default function Listing() {
               Link copied!
             </p>
           )}
+         
           <div className='flex flex-col max-w-4xl mx-auto p-3 my-7 gap-4'>
-            <p className='text-2xl font-semibold flex items-center'>
-              {listing.name} - <FaRupeeSign className='text-2xl  mx-0 font-semibold  flex flex-wrap items-center gap-4 sm:gap-6' />{' '}
-              {listing.offer
-                ? listing.discountPrice.toLocaleString('en-US')
-                : listing.regularPrice.toLocaleString('en-US')}
-              {listing.type === 'rent' && ' / month'}
-            </p>
+          <p className='text-2xl font-semibold flex items-center'>
+          {listing.name} - <FaRupeeSign className='text-2xl  mx-0 font-semibold  flex flex-wrap items-center gap-4 sm:gap-6' /> {' '}
+          {listing.offer ? (
+            <React.Fragment>
+             
+             
+               {listing.regularPrice - listing.discountPrice}
+             
+            </React.Fragment>
+          ) : (
+            listing.regularPrice.toLocaleString('en-US')
+          )}
+          {listing.type === 'rent' && ' / month'}
+        </p>
+        
             <p className='flex items-center mt-6 gap-2 text-slate-600  text-sm'>
               <FaMapMarkerAlt className='text-green-700' />
               {listing.address}
@@ -107,7 +117,7 @@ export default function Listing() {
               {listing.offer && (
                 <p className='bg-green-900 w-full max-w-[200px] text-white text-center p-1 rounded-md flex justify-center items-center'>
                   <FaRupeeSign className='text-white font-semibold text-sm mr-2' />
-                  {+listing.regularPrice - +listing.discountPrice} OFF
+                  {listing.discountPrice} OFF
                 </p>
               )}
             </div>
